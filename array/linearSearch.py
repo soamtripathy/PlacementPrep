6fbdd78ad3b52a231139ccfb8c130f234246fff6
Soumya Ranjan Tripathy <soumyaranjantripathy44@gmail.com>
2022-12-29 10:07:51 +0530

    binary search updated

--- array/linearSearch.py
+++ array/linearSearch.py
@@ -1,9 +1,21 @@
+#Advance python using if else
 list = [1, 5, 6, 8, 11, 34, 25]
-def binarySearch(num):
+def linearSearch(num):
     if num in list:
         return (f"{num} is present.")
     else:
         return ("You're on wrong list")
 x = int(input("Enter the number:"))
-y = binarySearch(x)
+y = linearSearch(x)
+print(y)
+'''------------------------------------------------------------------------------'''
+#traditional method of linear search
+def linearSearch(num):
+    for i in list:
+        if num == i:
+            return (f"{num} is present.")
+        
+    return ("You're wrong list")
+x = int(input("Enter the number:"))
+y = linearSearch(x)
 print(y)
