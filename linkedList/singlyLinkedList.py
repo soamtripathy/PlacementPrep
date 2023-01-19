edaa828938a59539091d5b98631e7cd55ecaa4ac
Soumya Ranjan Tripathy <soumyaranjantripathy44@gmail.com>
2023-01-19 23:27:29 +0530

    created : singly linkedlist.py

---
+++ linkedList/singlyLinkedList.py
@@ -0,0 +1,42 @@
+#create nodes
+#Create Linked List
+# Add nodes to Linked List
+#Print Linked List
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+    
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def insert(self,newNode):
+        #head=>John=>None
+        if self.head is None:
+            self.head = newNode
+        else:
+            #head=>John=>Ben=>None
+            lastNode = self.head
+            while True:
+                if lastNode.next is None:
+                    break
+                lastNode = lastNode.next
+            lastNode.next = newNode
+    def printList(self):
+        #head=>John=>Ben=>Mathews=>None
+        currentNode = self.head
+        while True:
+            if currentNode is None:
+                break
+            print(currentNode.data)
+            currentNode = currentNode.next 
+#firstNode.data => John, firstNode.next = None
+firstNode = Node("John")
+linkedList = LinkedList()
+linkedList.insert(firstNode)
+secondNode= Node("Ben")
+linkedList.insert(secondNode)
+thirdNode = Node("Mathews")
+linkedList.insert(thirdNode)
+linkedList.printList()
