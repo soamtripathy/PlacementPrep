229c4ea72c6570ba648496b13fbb0d1de0c22566
soamtripathy <soumyaranjantripathy44@gmail.com>
2023-04-21 23:33:17 +0530

    created: 1929-concatenation-of-array.py

---
+++ practice/1929-concatenation-of-array.py
@@ -0,0 +1,3 @@
+def getConcatenation(nums):
+        return 2 * nums
+print(getConcatenation([1, 2, 1]))
---
+++ practice/2469-convert-the-temperature.py
@@ -0,0 +1,3 @@
+def convertTemperature(celsius):
+        return [celsius+273.15, celsius*1.80 + 32.00]
+print(convertTemperature(37))
